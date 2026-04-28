8c1e019b3c977f020ffec456e5dc49beacc67a69
Samiksha194 <samikshabajesaria@gmail.com>
2026-04-28 20:33:50 +0545

    blahh

--- blah.py
+++ blah.py
@@ -1,5 +1,18 @@
 from tkinter import *
-window=Tk()
-window.title('Demo window')
-window.geometry('400x300')
-window.mainloop()
+root=Tk()
+root.title('Number pad')
+root.geometry('250x300')
+nums=[[9,8,7],[6,5,4],[3,2,1],['#',0,'*']]
+for i in range(4):
+    root.columnconfigure(i, weight=1, minsize=75)
+    root.rowconfigure(i, weight=1, minsize=50)
+    for j in range(0,3):
+        frame=Frame(
+            master=root,
+            relief=GROOVE,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j)
+        label= Label(master=frame, text=nums[i][j],bg='#d0efff')
+        label.pack(padx=3, pady=3)
+root.mainloop()
